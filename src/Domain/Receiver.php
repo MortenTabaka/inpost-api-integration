a0fa7f148ea15b8781591f6f57e9e015dbf8f08a
marcin.tabaka <marcin.tabaka.it@gmail.com>
2025-07-19 18:19:27 +0200

    clean up

--- src/Domain/Receiver.php
+++ src/Domain/Receiver.php
@@ -4,10 +4,7 @@ namespace Domain;
 
 class Receiver
 {
-    /*
-     * {"id":26590330,"street":"Polki","building_number":"1B","line1":null,"line2":null,"city":"Warszawa","post_code":"02-826","country_code":"PL"},"invoice_address":{"id":26590331,"street":"Polki","building_number":"1B","line1":null,"line2":null,"city":"Warszawa","post_code":"02-826","country_code":"PL"},"contact_person":null,"created_at":"2025-07-19T11:29:29.099+02:00","updated_at":"2025-07-19T11:29:29.347+02:00"}
-     */
-    public ?string $company_name;
+    public ?string $companyName;
     public ?string $firstName;
     public ?string $lastName;
     public ?string $email;
@@ -23,7 +20,7 @@ class Receiver
         ?string $phone,
         Address $address
     ) {
-        $this->company_name = $company_name;
+        $this->companyName = $company_name;
         $this->firstName = $firstName;
         $this->lastName = $lastName;
         $this->email = $email;
